41479e84d0bcb7af2fe8788daa6357fd3573ad18
Greg <kousagi1012@yahoo.com>
2017-04-25 15:46:39 -0700

    debug

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (request, response) => {
-  response.send('ET go home')
+  response.render('index')
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000)
