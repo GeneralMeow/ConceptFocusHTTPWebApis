6a0ac835a7eb064eee1f5ba4626713228cce9a60
Greg <kousagi1012@yahoo.com>
2017-04-27 13:32:59 -0700

    data from openweatherapi is coming in

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../database/database')
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 // get homepage
 router.get('/', (request, response) => {
@@ -28,8 +30,13 @@ router.post('/create-itinerary', (request, response, next) => {
 
 // get existing itinerary
 router.get('/itinerary', (request, response) => {
-  db.getDetails()
-  .then((details) => response.render('itinerary', {details}))
+  Promise.all([db.getDetails(), fetchWeather()])
+  // db.getDetails()
+  .then( (values) => {
+    const renameMe = {values}
+    console.log('renameMe====>', renameMe)
+    response.render('itinerary', renameMe)
+  })
 })
 
 router.get('/contact', (request, response) => {
@@ -43,5 +50,28 @@ router.get('/about', (request, response) => {
 router.get('/404', (request, response) => {
   response.render('404')
 })
+
+const fetchWeather = () => {
+  const fetchUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=2154624&APPID=253e1c4d472cec6a63788025b1f71efb'
+  return fetch(fetchUrl)
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+      }
+      // Examine the text in the response
+      return response.json()
+        .then(function(data) {
+          const weather = data.list[0].weather
+          return weather
+          // console.log(weather);
+        })
+      }
+    )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+}
 
 module.exports = router
