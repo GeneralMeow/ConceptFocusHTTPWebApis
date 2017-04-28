0dc71ef1f68e79ff8e155f13bf01f12d375b9c8f
Greg <kousagi1012@yahoo.com>
2017-04-28 15:24:02 -0700

    finish mock status code responses

--- database/database.js
+++ database/database.js
@@ -7,6 +7,10 @@ const saveItinerary = (airline, hotel, budget) => db.none(`INSERT INTO travel (h
 
 const getSuitcase = () => db.any(`SELECT * FROM suitcase ORDER BY id`)
 
+const packSuitcase = (thing) => db.any(`INSERT INTO suitcase (item) VALUES ($1)`, [thing])
+
 const updateSuitcase = (id, item) => db.any(`UPDATE suitcase SET item = $2 WHERE id = $1 RETURNING *`, [id, item])
 
-module.exports = { getDetails, saveItinerary, getSuitcase, updateSuitcase }
+const clearSuitcase = () => db.none(`DELETE * FROM suitcase`)
+
+module.exports = { getDetails, saveItinerary, getSuitcase, packSuitcase, updateSuitcase, clearSuitcase }
--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS travel_app;
 CREATE DATABASE travel_app;
 \c travel_app
 DROP TABLE IF EXISTS travel;
+DROP TABLE IF EXISTS suitcase;
 
 CREATE TABLE travel (
   id SERIAL PRIMARY KEY,
